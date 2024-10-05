f5b2238eaf003fcae9063e89c67cd09e234e9afc
Tolga-Tolga <oe.tolg@gmail.com>
2024-10-05 16:05:04 +0200

    feat: config declaration

--- Config.java
+++ Config.java
@@ -2,9 +2,9 @@
  * Config
  */
 public class Config {
-    public static int PLAYFIELD_SIZE_X = 10;
-    public static int PLAYFIELD_SIZE_Y = 5;
-    public static int PLAYER_TURN = 1;
+    public static int PLAYFIELD_SIZE_X = 10;    //needs to be at least 4
+    public static int PLAYFIELD_SIZE_Y = 5;     //needs to be at least 4
+    public static int PLAYER_TURN = 1;          //1 for player 1 or 2 for player 2
     public static char PLAYER_1_SYMBOL = 'X';
     public static char PLAYER_2_SYMBOL = 'O';
 }
